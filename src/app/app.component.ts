aa46e0fde894b692bddf9e8b4cfbd06d29ecf64b
nvtdev <nikolayivanov91@gmail.com>
2017-11-14 22:34:13 +0200

    Added coins table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
+import { HttpClient } from "@angular/common/http";
+import { log } from "util";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "app";
+  coins: Object;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // this.getCoins(null);
+    this.getReddit(null);
+  }
+
+  getCoins(params): void {
+    this.http
+      .get("https://api.coinmarketcap.com/v1/ticker/?limit=50")
+      .subscribe(data => {
+        this.coins = data;
+        console.log(this.coins);
+      });
+  }
+
+  getReddit(params): void {
+    this.http
+      .get("http://www.reddit.com/search.json?q=bitcoin")
+      .subscribe(data => {
+        console.log(data);
+      });
+  }
+
+  updateCoins(): void {
+    this.getCoins(null);
+    setTimeout(function() {
+      this.updateCoins();
+    }, 1000);
+  }
 }
